210fe4781196cff5a77f45453085e6c46b560b9a
AndreaNova1 <69744573+andreanova1@users.noreply.github.com>
2022-04-09 08:16:44 -0600

    se crean vistas y modelos para controlar el inicio de sesion

--- public/index.php
+++ public/index.php
@@ -44,7 +44,7 @@ require __DIR__.'/../vendor/autoload.php';
 |
 */
 
-$app = require_once __DIR__.'/../bootstrap/app.php';
+$app = require_once __DIR__ . '/../bootstrap/error 404.php';
 
 $kernel = $app->make(Kernel::class);
 
